48d33e41a207f34fa43405ba7c117effa5b5ecbf
abhinay kumar suraj <abhinaykumarsuraj2012@gmail.com>
2022-05-16 00:41:32 +0530

    Time: 0 ms (100.00%), Space: 42 MB (83.97%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,32 +1,23 @@
 class Solution {
     
-    public boolean bs(int[][] nums, int target,int left,int right)
-    {
-        if(right>=left)
-        {
-        int mid=left+(right-left)/2;
-        
-        if((nums[mid][0]<target && nums[mid][nums[0].length-1]>target)||(nums[mid][0]==target)||(nums[mid][nums[0].length-1]==target))
-        {
-            for(int i=0;i<nums[0].length;i++)
-            {
-                if(nums[mid][i]==target)
-                    return true;
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int r = 0;
+        int c = col - 1;
+        // Binary Search
+        while (r < row && c >= 0) {
+            int current = matrix[r][c];
+            if (current == target) {
+                return true;
             }
-            // return false;
+            if (current > target) {
+                c--;
+            }
+            else {
+                r++;
+            }
         }
-        if(nums[mid][nums[0].length-1]<target)
-          return  bs(nums,target,mid+1,right);
-         return bs(nums,target,left,mid-1);
-        // return -1;
-        }
-        else
-            return false;
-    }
-    
-    public boolean searchMatrix(int[][] matrix, int target) {
-        return bs(matrix,target,0,matrix.length-1);
-        // System.out.println((matrix.length-1)+" "+(matrix[0].length-1));
-        // return true;
+        return false;
     }
 }
