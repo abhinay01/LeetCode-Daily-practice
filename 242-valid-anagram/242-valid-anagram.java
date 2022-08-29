bbcd9a8cc1ea9e9ffd17148d8a200ca81a4c9ee4
abhinay kumar suraj <abhinaykumarsuraj2012@gmail.com>
2022-08-30 01:44:40 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (99.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int count1[]=new int[26];
-        int count2[]=new int[26];
-        Arrays.fill(count1,0);
-        Arrays.fill(count2,0);
-        for(int i=0;i<s.length();i++)
+        if(s.length()!=t.length())
+            return false;
+        char [] c=s.toCharArray();
+        char [] check=t.toCharArray();
+        int [] cb= new int[26];
+        // int [] checkb = new int[s.size()];
+        for(int i=0;i<c.length;i++)
         {
-            count1[(s.charAt(i)-'a')%26]++;
+            cb[c[i]-'a']++;
         }
-        for(int i=0;i<t.length();i++)
+        for(int i=0;i<c.length;i++)
         {
-            count2[(t.charAt(i)-'a')%26]++;
+            cb[check[i]-'a']--;
         }
         for(int i=0;i<26;i++)
         {
-            if(count1[i]!=count2[i])
+            if(cb[i]!=0)
                 return false;
         }
         return true;
