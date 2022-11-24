b70d4a6853ddfea77467e7f7850646c079a9df3e
abhinay kumar suraj <abhinaykumarsuraj2012@gmail.com>
2022-11-24 09:50:58 +0530

    Time: 4 ms (8.07%), Space: 42.9 MB (39.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,27 @@
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=0,j=0;
-        while(i<m+n && j<n)
-        {
-            if(nums1[i]==0 && i>=m)
-            {
-                nums1[i]=nums2[j];
+    public void shellSort(int [] v, int n){
+        int gap =(int) Math.ceil(n/2);
+        while(gap>=1){
+            int i=0, j=gap;
+            while(j<n){
+                if(v[i]>v[j])
+                {
+                    int t=v[i];
+                    v[i]=v[j];
+                    v[j]=t;
+                    // swap(v[i], v[j]);
+                }
                 i++;
                 j++;
             }
-            else if(nums1[i]<=nums2[j])
-            {
-                i++;
-            }
-            else
-            {
-                int t=nums1[i];
-                nums1[i]=nums2[j];
-                nums2[j]=t;
-                for(int r=j+1;r<n;r++)
-                {
-                    if(nums2[r]<nums2[r-1])
-                    {
-                        int t1=nums2[r];
-                        nums2[r]=nums2[r-1];
-                        nums2[r-1]=t1;
-                    }
-                }
-                i++;
-            }
+            if (gap==1) return;
+            gap =(int) Math.ceil((float)gap/2);
         }
+    }
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m, j=0; j<n; i++, j++)
+            nums1[i] = nums2[j];
+        shellSort(nums1, nums1.length);
     }
 }
